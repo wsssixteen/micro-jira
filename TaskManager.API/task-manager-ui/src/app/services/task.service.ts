ee2c37e71a0a36403441b8a0c6e0b16918e36f2c
Ahmad Ridhwan Anuar <vice44wsssixteen@gmail.com>
2025-11-13 05:51:25 +0800

    Update task.service.ts

--- TaskManager.API/task-manager-ui/src/app/services/task.service.ts
+++ TaskManager.API/task-manager-ui/src/app/services/task.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient,HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 
 export interface TaskItem {
   id?: number;
