1fa43e82674e048844230bcfab8c41989ddfee6a
wsssixteen <vice44wsssixteen@gmail.com>
2025-11-20 08:33:42 +0800

    Update config.service.ts for Github build job.

--- TaskManager.API/task-manager-ui/src/app/services/config.service.ts
+++ TaskManager.API/task-manager-ui/src/app/services/config.service.ts
@@ -20,11 +20,11 @@ export class ConfigService {
 
     try {
       // Try local fonfig first
-      this.config = await firstValueFrom(this.http.get<AppConfig>('/config.local.json'));
+      this.config = await firstValueFrom(this.http.get<AppConfig>('/config.json'));
       //return this.config;
     } catch (error) {
       // Fallback to production config
-      this.config = await firstValueFrom(this.http.get<AppConfig>('/config.json'));
+      this.config = await firstValueFrom(this.http.get<AppConfig>('/config.local.json'));
       //return this.config;
     }
     return this.config;
